03cb04b72402bcb5c412a8f4b5740c7548f1fc5b
ainyan03 <lovyan03ai@gmail.com>
2025-04-03 16:33:59 +0900

    feat: Add C++ compatibility layer in base/cpp

---
+++ src/flexhal/base/cpp/__include.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "compat.hpp"
+// 将来的に追加される他のC++互換ファイル用
+// #include "optional.hpp"
+// など
---
+++ src/flexhal/base/cpp/compat.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+namespace flexhal::base::cpp {
+
+// C++のバージョンチェック
+#if defined(__cplusplus)
+  #if __cplusplus >= 201703L
+    #define FLEXHAL_INTERNAL_CPP17
+  #endif
+  #if __cplusplus >= 202002L
+    #define FLEXHAL_INTERNAL_CPP20
+  #endif
+#endif
+
+// [[nodiscard]] の互換性対応
+#ifdef FLEXHAL_INTERNAL_CPP17
+  #define FLEXHAL_INTERNAL_NODISCARD [[nodiscard]]
+#else
+  #define FLEXHAL_INTERNAL_NODISCARD
+#endif
+
+// C++17 以降の機能を使用するかどうかのフラグ
+#ifdef FLEXHAL_INTERNAL_CPP17
+  #define FLEXHAL_INTERNAL_USE_CPP17_FEATURES
+#endif
+
+} // namespace flexhal::base::cpp
